2d71688cf481eea7c9bf1d02eeedd30c99e815c0
hugosandsjo <hugosandsjo@gmail.com>
2024-03-20 21:15:30 +0100

    connected

--- server/configs/database.js
+++ server/configs/database.js
@@ -2,15 +2,28 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
+// standard mysql connection
+// const pool = mysql
+//   .createPool({
+//     host: "127.0.0.1",
+//     user: "root",
+//     password: "",
+//     database: "yrgoeventDB",
+//   })
+//   .promise();
+
 const pool = mysql
   .createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "yrgoeventDB",
+    host: "db-mysql-lon1-26103-do-user-16098185-0.c.db.ondigitalocean.com",
+    user: "doadmin",
+    password: "AVNS_QjIchW2TZrOVugX4VOQ",
+    database: "defaultdb",
+    port: 25060,
+    ssl: { rejectUnauthorized: false }, // this is required if the sslmode is REQUIRED
   })
   .promise();
 
+// connection using .env
 // const pool = mysql
 //   .createPool({
 //     host: process.env.MYSQL_HOST,
@@ -20,14 +33,25 @@ const pool = mysql
 //   })
 //   .promise();
 
-async function getStudents() {
+export async function getStudents() {
   const [rows] = await pool.query("SELECT * from students");
   return rows;
 }
-const students = await getStudents();
-console.log(students);
 
-async function getStudent() {
+export async function getVisitors() {
+  const [rows] = await pool.query("SELECT * from visitors");
+  return rows;
+}
+
+export async function getUsers() {
+  const result = await pool.query(`
+  SELECT name
+  FROM users
+  `);
+  return result;
+}
+
+export async function getStudent() {
   const [rows] = await pool.query(`
   SELECT *
   FROM students
@@ -36,5 +60,11 @@ async function getStudent() {
   return rows[0];
 }
 
-const john = await getStudent();
-console.log(john);
+export async function createStudent(area, name, company) {
+  const result = await pool.query(
+    `INSERT INTO visitors (area, name, company)
+  VALUES (? , ? , ?)`,
+    [area, name, company],
+  );
+  return result;
+}
