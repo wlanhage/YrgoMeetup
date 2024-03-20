68a86927753b6da4a4cca2713a6cf6ace56e1068
wlanhage <wlanhage@gmail.com>
2024-03-20 21:09:25 +0100

    added base form structure

---
+++ client/src/pages/reg.jsx
@@ -0,0 +1,16 @@
+ //Reg.js
+ import React from 'react'
+ import Navbar from '../components/navbar';
+import Registrationform from '../components/Registrationform';
+
+ const Reg = () => {
+   return (
+     <div>
+         <Navbar />
+         <Registrationform />
+         <h1>Reg</h1>
+     </div>
+   )
+ }
+
+ export default Reg
