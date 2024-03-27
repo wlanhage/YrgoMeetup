3851ef9dde38c5861504f14bd5559d68f6c5a50f
hugosandsjo <hugosandsjo@gmail.com>
2024-03-27 11:26:20 +0100

    added mime-type in index.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
 app.get("/companys", async (req, res) => {
   const companys = await getCompanys();
