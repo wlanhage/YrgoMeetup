5efddfc45974121a8580241be55e5f0c0637ee08
wlanhage <wlanhage@gmail.com>
2024-04-02 10:42:34 +0200

    fixed styling on CompanyCard page, buttons, and added progressBar component and its logic to page.

--- client/src/components/CompanyCardContent.jsx
+++ client/src/components/CompanyCardContent.jsx
@@ -1,18 +1,92 @@
+import React from 'react';
 import CompanyCardDesign from "../pages/CompanyCardDesign";
+import '../App.css';
 
 
 function CompanyCardContent (props) {
     
+    const cardContainer = {
+        display: 'flex',
+        textAlign: 'start',
+        
+        flexDirection: 'row',
+
+        padding: '12px',
+
+        fontSize: '12px',
+    }
+
+    const cardContainerLeft = {
+        display: 'flex',
+        gap: '19px',
+        flexDirection: 'column',
+
+    }
+
+    const cardContainerRight = {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '172px',
+        marginLeft: '10px',
+        gap: '21.5px',
+        
+    }
+
+    const imageContainer = {
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'gray',
+
+    }
+
+    const tagsArea = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+
+    }
+
+    const tag = {
+        padding: '3px',
+        margin: '2px',
+        border: 'solid',
+    }
+
+    const text = {
+        fontFamily: 'inter',
+
+        border: '1px solid black',
+        
+        paddingLeft: '3px',
+
+    }
+
 
 
     return (
-        <>
-        <div>
-        <p>hej</p>
-        <div>{props.companyName}</div>
-        <p>hej</p>
+        <div style={cardContainer}>
+                <div style={cardContainerLeft}>
+                    <div style={imageContainer}>
+
+                    </div>
+                    <div style={text}>Company name</div>
+                    <div style={text}>Webpage.se</div>
+                </div>
+                <div style={cardContainerRight}>
+                    <div style={text}>Kontaktperson</div>
+                    <div style={text}>Mailadress@mail.se</div>
+                    <div style={text}>Företaget gör ABC...</div>
+                    <div style={text}>Något annat vi bör veta</div>
+                    <div style={tagsArea}>
+                        <div style={tag}>Ett</div>
+                        <div style={tag}>Två</div>
+                        <div style={tag}>Tre</div>
+                        <div style={tag}>Fyra</div>
+                    </div>
+
+                </div>
         </div>
-        </>
     )
 }
 
--- client/src/pages/CompanyCardDesign.jsx
+++ client/src/pages/CompanyCardDesign.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import "../App.css";
+import RedButton from '../components/RedButton';
+import CompanyCardContent from '../components/CompanyCardContent';
+import CompanyRegProgBar from '../components/CompanyRegProgBar';
 
 function CompanyCardDesign () {
 // FUNKTIONER -------------
+    const [selectedTitle, setSelectedTitle] = useState('Färg');
 
-    const [cardColor, setCardColor] = useState('red');
-
+    const [cardColor, setCardColor] = useState('#F52A3B');
     const handleColorChange = (event) => {
         setCardColor(event.target.value)
     };
@@ -14,27 +19,68 @@ function CompanyCardDesign () {
         setEmoji(event.target.value);
     }
 
+    const [showColorButtons, setShowColorButtons] = useState(true);
+   /*  const [showPatternButtons, setShowPatternButtons] = useState(false); */
+    const [showIconButtons, setShowIconButtons] = useState(false);
+
+    const handleColorClick = () => {
+      setSelectedTitle('Färg');
+      setShowColorButtons(!showColorButtons);
+      /* setShowPatternButtons(false); */
+      setShowIconButtons(false);
+    }
+    /* const handlePatternClick = () => {
+        setShowPatternButtons(!showPatternButtons);
+    } */
+    const handleIconClick = () => {
+      setSelectedTitle('Ikon');
+      setShowIconButtons(!showIconButtons);
+      setShowColorButtons(false);
+      /* setShowPatternButtons(false); */
+    }
+
 
     // STYLING -------------
 
+    const headerText = {
+        fontFamily: 'inter',
+        fontSize: '34px',
+        width: '320px',
+
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '70px',
+        marginBottom: '40px',
+        textAlign: 'start',
+    }
+
     const card = {
         width: '320px',
-        height: '180px',
+        height: '200px',
         backgroundColor: cardColor,
-
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        border: '2.5px solid',
+        borderRadius: '9px 9px 9px 9px',
+        
     } 
 
     const selectionBar = {
-        marginTop: '50px',
+        marginTop: '20px',
+        marginBottom: '0px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
 
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
+        width: '310px',
 
         padding: '20px',
-        borderRadius: '5px, 5px, 5px, 5px',
-        backgroundColor: 'gray',
     }
+
+   
 
     const emojiStyle = {
         fontSize: '50px',
@@ -43,41 +89,175 @@ function CompanyCardDesign () {
         right: '70px',
     }
 
+    const radioButton = {
+        width: '50px',
+        height: '50px',
+        margin: '15px',
+        border: 'none',
+        backgroundColor: '#D9D9D9',
+        /* outline: 'none',
+        appearance: 'none',
+        WebkitAppearance: 'none',
+        MozAppearance: 'none' */
+    }
+
+    const hiddenButton = {
+        display: 'none',
+    }
+
+    const buttonContainer = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '10px',
+
+        marginTop: '70px',
+        
+    }
+
+
+
+
     // FRONT END KOD -------------
 
     return (
         <>
-        <h1>Designa ditt egna kort</h1>
+            <Navbar />
+            <CompanyRegProgBar number={'1'} redBarWidth={'110px'} grayBarWidth={'220px'} />
 
+        <div style={headerText}>Designa ditt <br/> egna visitkort</div>
+
+        {<div style={emojiStyle}>
+                    {emoji}
+                </div> }
 
         <div style={card}>
-            <div>
-                <div style={emojiStyle}>
-                    {emoji}
-                </div>
-            </div>
+            <CompanyCardContent />
         </div>
 
 
         <div style={selectionBar}>
-            <div>Färg
-                <select  onChange={handleColorChange}>
-                    <option value="black">Svart</option>
-                    <option value="red">Röd</option>
-                    <option value="yellow">Gul</option>
-                    <option value="Blue">Blå</option>
-                </select>
-            </div>
-            <div>Mönster</div>
-            <div>Ikon
-            <select  onChange={handleIconChange}>
-                    <option value="&#128507;">Emoji 1</option>
-                    <option value="&#128511;">Emoji 2</option>
-                    <option value="&#128508;">Emoji 3</option>
-                </select>
-            </div>
+        <div 
+          className="selectionBarTitle" 
+          onClick={handleColorClick} 
+          style={{ width:'70px', borderBottom: '3px solid', borderBottomColor: selectedTitle === 'Färg' ? 'red' : 'black', }}
+        >
+          Färg
         </div>
-        
+        <div 
+          className="selectionBarTitle" 
+          /* onClick={handlePatternClick}  */
+          style={{ width:'70px', borderBottom: '3px solid', borderBottomColor: selectedTitle === 'Mönster' ? 'red' : 'black', }}
+        >
+          Mönster
+        </div>
+        <div 
+          className="selectionBarTitle" 
+          onClick={handleIconClick} 
+          style={{ width:'70px', borderBottom: '3px solid', borderBottomColor: selectedTitle === 'Ikon' ? 'red' : 'black', }}
+        >
+          Ikon
+        </div>
+        </div>
+
+
+        {showColorButtons && (
+        <div>
+            <input
+            style={radioButton}
+            type="radio"
+            name="color"
+            value="white"
+            onChange={handleColorChange}
+          />
+        <input
+            style={radioButton}
+            type="radio"
+            name="color"
+            value="#F52A3B"
+            onChange={handleColorChange}
+          />
+          <input 
+            style={radioButton}
+            type="radio"
+            name="color"
+            value="blue"
+            onChange={handleColorChange}
+          />
+          <input
+            style={radioButton}
+            type="radio"
+            name="color"
+            value="green"
+            onChange={handleColorChange}
+          />
+        </div>
+        )}
+
+
+        {/* <div style={hiddenButton}>
+        <input
+            style={radioButton}
+            type="radio"
+            name="pattern"
+            value=""
+            onChange={}
+          />
+          <input
+            style={radioButton}
+            type="radio"
+            name="color"
+            value=""
+            onChange={}
+          />
+          <input
+            style={radioButton}
+            type="radio"
+            name="color"
+            value=""
+            onChange={}
+          />
+        </div> */}
+
+    {showIconButtons && (
+        <div >
+            <input
+            style={radioButton}
+            type="radio"
+            name="Icon"
+            value=""
+            onChange={handleIconChange}
+          />
+        <input
+            style={radioButton}
+            type="radio"
+            name="Icon"
+            value="&#128507;"
+            onChange={handleIconChange}
+            
+          />
+          <input
+            style={radioButton}
+            type="radio"
+            name="Icon"
+            value="&#128511;"
+            onChange={handleIconChange}
+          />
+          <input
+            style={radioButton}
+            type="radio"
+            name="Icon"
+            value="&#128508;"
+            onChange={handleIconChange}
+          />
+        </div>
+        )}
+
+        <div style={buttonContainer}>
+            <RedButton text={'Skapa'}/>
+            <RedButton text={'Backa'} style={{ backgroundColor: 'white', border: '1px solid red', color: 'red', }}/>
+        </div>
         </>
     )
 }
