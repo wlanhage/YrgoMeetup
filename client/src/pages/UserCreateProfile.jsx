a387471d2cd99b415bc32263f390b09fc64a4097
hugosandsjo <hugosandsjo@gmail.com>
2024-04-12 16:53:11 +0200

    started styling second step student reg

--- client/src/pages/UserCreateProfile.jsx
+++ client/src/pages/UserCreateProfile.jsx
@@ -6,6 +6,9 @@ import ReturnButton from "../components/ReturnButton";
 import SecondaryButton from "../components/SecondaryButton";
 import { useEffect, useState } from "react";
 import axios from "axios";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 //user info page. later this will fetch the user's name, area etc from the database and display their info here
 
@@ -35,6 +38,46 @@ const UserCreateProfile = () => {
     marginLeft: "2rem",
     marginRight: "2rem",
   };
+
+  const label = {
+    fontSize: "16px",
+    color: "black",
+    fontFamily: "inter",
+    textAlign: "left",
+    marginLeft: "10px",
+  };
+
+  const input = {
+    backgroundColor: "#ffffff",
+    padding: "10px",
+    fontSize: "16px",
+    color: "black",
+    fontFamily: "inter",
+    marginBottom: "20px",
+    textAlign: "left",
+    border: "1px solid #000000",
+    borderRadius: "4px, 4px, 4px, 4px",
+  };
+
+  const [formData, setFormData] = useState({
+    linkedin: "",
+    portfolio: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, type, checked } = e.target;
+    let value = e.target.value;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const breakpoints = [576, 768, 900, 1200];
+
+  const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+
   /* useEffect(() => {
     const getSkills = async () => {
     try {
@@ -51,16 +94,176 @@ const UserCreateProfile = () => {
       <Navbar />
       <h1 style={header}>Skapa Profil</h1>
       <form action="" style={basicStyle}>
+        <label htmlFor="" style={label}>
+          Förnamn
+        </label>
+        <input
+          type="text"
+          name="linkedin"
+          style={input}
+          css={css`
+            ${mq[2]} {
+              width: 50%;
+            }
+          `}
+          value={formData.linkedin}
+          onChange={handleChange}
+          placeholder="Förnamn"
+          required
+        />
+
+        <label htmlFor="" style={label}>
+          Efternamn
+        </label>
+
+        <input
+          type="text"
+          name="portfolio"
+          style={input}
+          css={css`
+            ${mq[2]} {
+              width: 50%;
+            }
+          `}
+          value={formData.portfolio}
+          onChange={handleChange}
+          placeholder="Efternamn"
+          required
+        />
         <p>Vilka kunskaper har du?</p>
-        <p>Jag vill synas för företagen</p>
+
+        <section
+          css={css`
+            display: flex;
+            flex-wrap: wrap;
+            width: 100%;
+
+            /* background-color: #ae6363; */
+          `}
+        >
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              PHP
+            </label>
+          </div>
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              C#
+            </label>
+          </div>
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              Laravel
+            </label>
+          </div>
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              Javascript
+            </label>
+          </div>
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              HTML
+            </label>
+          </div>
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              CSS
+            </label>
+          </div>
+          <div
+            css={css`
+              margin-bottom: 20px;
+            `}
+          >
+            <input
+              type="checkbox"
+              name="designer"
+              checked={formData.designer}
+              onChange={handleChange}
+            />
+            <label htmlFor="designer" style={label}>
+              Python
+            </label>
+          </div>
+        </section>
+        {/* <p>Jag vill synas för företagen</p>
         <div style={style}>
           <label htmlFor="Ja/nej">Ja</label>
           <input type="radio" name="Ja/Nej" value="Ja" />
           <label htmlFor="Ja/Nej">Nej</label>
           <input type="radio" name="Ja/Nej" value="Nej" />
+        </div> */}
+        <div
+          css={css`
+            margin-bottom: 1rem;
+          `}
+        >
+          <RedButton text="Skapa konto" />
         </div>
-        <RedButton text="Färdigställ Profil" />
-        <SecondaryButton text="Logga ut" />
+        <div>
+          <SecondaryButton text="Logga ut" />
+        </div>
       </form>
     </div>
   );
